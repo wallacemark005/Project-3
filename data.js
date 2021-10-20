5afa936cfb6453b57df1d66aac610f5e123d00ba
wallacemark005 <85199650+wallacemark005@users.noreply.github.com>
2021-10-19 20:03:00 -0400

    Project 3 Submission

---
+++ data.js
@@ -0,0 +1,38 @@
+var data = [
+  {
+    Date: "05/27/2021",
+    Close_Price: 26.52,
+    Volume: 705545700,
+    NASDAQ: 13736.28027,
+  }, {
+    Date: "05/28/2021",
+    Close_Price: 26.12,
+    Volume: 660623600,
+    NASDAQ: 13748.74023,
+  }, {
+    Date: "06/01/2021",
+    Closing_Price: 32.04,
+    Volume: 508694600,
+    NASDAQ: 13736.480469,
+  }, {
+    Date: "06/02/2021",
+    Closing_Price: 62.55,
+    Volume: 766462500,
+    NASDAQ: 13756.330078,
+  }, {
+    Date: "06/03/2021",
+    Closing_Price: 51.34,
+    Volume: 598142200,
+    NASDAQ: 13614.509766,
+  }, {
+    Date: "06/04/2021",
+    Closing_Prce: 47.91,
+    Volume: 337710100,
+    NASDAQ: 13814.490234,
+  }, {
+    Date: "06/07/2021",
+    Closing_Price: 55.00,
+    Volume: 349094900,
+    NASDAQ: 13881.719727,
+  }
+];
---
+++ index.js
@@ -0,0 +1,5 @@
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
